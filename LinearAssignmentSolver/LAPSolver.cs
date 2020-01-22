61adfd403a36a34b7e9f013ea688546e6ec5657e
Kazimierz Wojciechowski <wojciechowski.kazimierz.it@gmail.com>
2020-01-22 02:01:25 +0100

    add the LAP solver
    tweaked to solve for the floating point case

---
+++ LinearAssignmentSolver/LAPSolver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinearAssignmentSolver
+{
+    public static class LAPSolver
+    {
+        public static int[] SolveAssignment(double[,] costMatrix)
+            => new HungarianAlgorithm(costMatrix).Run();
+        public static double AssignmentCost(double[,] costMatrix, int[] assignment)
+        {
+            var sum = .0;
+            for (int i = 0; i < assignment.Length; i++)
+                sum += costMatrix[i, assignment[i]];
+            return sum;
+        }
+    }
+}
