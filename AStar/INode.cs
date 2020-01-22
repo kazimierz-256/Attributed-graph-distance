04280ee06543d80655277d79737b22df9c327ed4
Kazimierz Wojciechowski <wojciechowski.kazimierz.it@gmail.com>
2020-01-22 01:57:17 +0100

    add approximation schemes

--- AStar/INode.cs
+++ AStar/INode.cs
@@ -4,9 +4,14 @@ using System.Text;
 
 namespace AStar
 {
+    public enum UpperBoundApproximationType
+    {
+        Greedy,
+        Random
+    }
     public interface INode : IComparable
     {
-        double UpperBound();
+        double UpperBound(UpperBoundApproximationType lowerBoundApproximationType = UpperBoundApproximationType.Best);
         double LowerBound();
         ICollection<INode> Expand();
     }
