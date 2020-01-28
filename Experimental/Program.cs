ec83f15f1faae1da7af2608a370b6e7e798a2826
Kazimierz Wojciechowski <wojciechowski.kazimierz.it@gmail.com>
2020-01-28 20:05:30 +0100

    set up experimental framework

--- Experimental/Program.cs
+++ Experimental/Program.cs
@@ -1,5 +1,9 @@
-﻿using RandomGraphProvider;
+﻿using AStarGraphNode;
+using RandomGraphProvider;
 using System;
+using System.Collections.Generic;
+using KNNClassifier;
+using AttributedGraph;
 
 namespace Experimental
 {
@@ -17,6 +21,16 @@ namespace Experimental
                 return random.NextDouble();
             };
 
+            Func<double, double, double> vertexAttributeMetric = (a1, a2) =>
+            {
+                return Math.Abs(a1 - a2);
+            };
+
+            Func<double, double, double> edgeAttributeMetric = (a1, a2) =>
+            {
+                return Math.Abs(a1 - a2);
+            };
+
             var G = RandomGraphFactory.generateRandomInstance(
                 vertices: 10,
                 density: .6,
@@ -24,16 +38,46 @@ namespace Experimental
                 vertexAttributeGenerator: vertexAttributeGenerator,
                 edgeAttributeGenerator: edgeAttributeGenerator
                 );
+            var graphsPreclassified = new List<(Graph<int, double, double>, int)>();
+            const int hGraphCount = 100;
+            for (int i = 0; i < hGraphCount; i++)
+            {
+                var H = RandomGraphFactory.generateRandomInstance(
+                    vertices: 13,
+                    density: .355,
+                    directed: true,
+                    vertexAttributeGenerator: vertexAttributeGenerator,
+                    edgeAttributeGenerator: edgeAttributeGenerator
+                    );
+                var hClassID = H.EdgeCount > 54 ? 1 : 0;
+                graphsPreclassified.Add((H, hClassID));
+            }
 
-            var H = RandomGraphFactory.generateRandomInstance(
-                vertices: 13,
-                density: .355,
-                directed: true,
-                vertexAttributeGenerator: vertexAttributeGenerator,
-                edgeAttributeGenerator: edgeAttributeGenerator
-                );
+            var distanceDictionary = new Dictionary<(double, double), double>();
+            foreach (var (vertex, attribute) in G.Vertices)
+            {
 
+            }
 
+            var a = new List<double>() { -1, 2, 0, 1, .5 };
+            var b = new List<double>() { -1, 2, 0, 1, .5 };
+
+            var k = 3;
+
+            var encodingMethod = GraphEncodingMethod.Wojciechowski;
+
+            var gClassID = KNNClassifier.KNNClassifier.Classify<int, double, double>(
+                k: k,
+                G,
+                graphsPreclassified,
+                vertexAttributeMetric,
+                edgeAttributeMetric,
+                a,
+                b,
+                encodingMethod: encodingMethod
+            );
+
+            System.Console.WriteLine($"Detected class: {gClassID}");
         }
     }
 }
