d161a17a5c7fd058117d7b6f440a96e9e2d10bb0
Kazimierz Wojciechowski <wojciechowski.kazimierz.it@gmail.com>
2020-01-29 18:18:45 +0100

    add initial interoperability with the database

--- KNNClassifierExperimental/Program.cs
+++ KNNClassifierExperimental/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using Postgres_enron_database.Data;
 
 namespace KNNClassifierExperimental
 {
@@ -6,7 +8,12 @@ namespace KNNClassifierExperimental
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using var context = new EnronContext();
+            var dateFrom = DateTime.Parse("2002 Dec 01");
+            var dateTo = DateTime.Parse("2002 Dec 01");
+            Console.WriteLine(context.Emails.Select(
+                email => email.SendDate < dateTo && email.SendDate > dateFrom
+                ).Count());
         }
     }
 }
