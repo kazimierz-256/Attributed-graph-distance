2ddf6b48984468bce06ae4c4f592a541f0f533c4
Kazimierz Wojciechowski <wojciechowski.kazimierz.it@gmail.com>
2020-01-29 16:39:46 +0100

    add a dual maximum assignment solver

--- LinearAssignmentSolver/LAPSolver.cs
+++ LinearAssignmentSolver/LAPSolver.cs
@@ -8,6 +8,18 @@ namespace LinearAssignmentSolver
     {
         public static int[] SolveAssignment(double[,] costMatrix)
             => new HungarianAlgorithm(costMatrix).Run();
+        public static int[] SolveAssignmentMax(double[,] costMatrix)
+        {
+            var negatedWeights = new double[costMatrix.GetLength(0), costMatrix.GetLength(1)];
+            for (int i = 0; i < costMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < costMatrix.GetLength(1); j++)
+                {
+                    negatedWeights[i, j] = -costMatrix[i, j];
+                }
+            }
+            return new HungarianAlgorithm(negatedWeights).Run();
+        }
         public static double AssignmentCost(double[,] costMatrix, int[] assignment)
         {
             var sum = .0;
