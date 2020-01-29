f9b8324cc72ae0a46597df9ef04732a5a1924033
Kazimierz Wojciechowski <wojciechowski.kazimierz.it@gmail.com>
2020-01-29 02:24:09 +0100

    add graph transformations

---
+++ AttributedGraph/Transform.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Linq;
+
+namespace AttributedGraph
+{
+    public static class Transform
+    {
+        public static Graph<V, VA, EA> Permute<V, VA, EA>(
+                Graph<V, VA, EA> graph, Random random = null,
+                Func<V, VA, (V, VA)> cloneVertex = null,
+                Func<(V, V), EA, ((V, V), EA)> cloneEdge = null
+            )
+        {
+            if (random == null)
+            {
+                random = new Random();
+            }
+            var vertices = graph.Vertices.ToList();
+            var indexes = new int[graph.VertexCount];
+            var randomNumbers = new int[graph.VertexCount];
+            for (int i = 0; i < graph.VertexCount; i++)
+            {
+                indexes[i] = i;
+                randomNumbers[i] = random.Next();
+            }
+            Array.Sort(randomNumbers, indexes);
+
+            var newGraph = new Graph<V, VA, EA>(graph.Directed);
+
+            foreach (var kvp in graph.Vertices)
+            {
+                var newVertex = kvp.Key;
+                var newVertexAttribute = kvp.Value;
+                if (cloneVertex != null)
+                    (newVertex, newVertexAttribute) = cloneVertex(kvp.Key, kvp.Value);
+                newGraph.AddVertex(newVertex, newVertexAttribute);
+            }
+
+            for (int i = 0; i < graph.VertexCount; i++)
+            {
+                for (int j = 0; j < graph.VertexCount; j++)
+                {
+                    var edge = (vertices[i].Key, vertices[j].Key);
+                    if (graph.ContainsEdge(edge))
+                    {
+                        var newEdgeAttribute = graph[edge];
+                        var newEdge = edge;
+                        if (cloneEdge != null)
+                            (newEdge, newEdgeAttribute) = cloneEdge(edge, newEdgeAttribute);
+                        newGraph.AddEdge((vertices[indexes[i]].Key, vertices[indexes[j]].Key), newEdgeAttribute);
+                    }
+                }
+            }
+
+            return newGraph;
+        }
+        public static Graph<V, VA, EA> Clone<V, VA, EA>(
+                Graph<V, VA, EA> graph,
+                Func<V, VA, (V, VA)> cloneVertex = null,
+                Func<(V, V), EA, ((V, V), EA)> cloneEdge = null
+            )
+        {
+            var newGraph = new Graph<V, VA, EA>(graph.Directed);
+
+            foreach (var kvp in graph.Vertices)
+            {
+                var newVertex = kvp.Key;
+                var newVertexAttribute = kvp.Value;
+                if (cloneVertex != null)
+                    (newVertex, newVertexAttribute) = cloneVertex(kvp.Key, kvp.Value);
+                newGraph.AddVertex(newVertex, newVertexAttribute);
+            }
+
+            foreach (var kvp in graph.Edges)
+            {
+                var newEdge = kvp.Key;
+                var newEdgeAttribute = kvp.Value;
+                if (cloneEdge != null)
+                    (newEdge, newEdgeAttribute) = cloneEdge(kvp.Key, kvp.Value);
+                newGraph.AddEdge(newEdge, newEdgeAttribute);
+            }
+
+            return newGraph;
+        }
+    }
+}
