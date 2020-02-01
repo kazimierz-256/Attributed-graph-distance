3a4eaaf3f055bbad3548b8f80bc81159afb32f2a
Kazimierz Wojciechowski <wojciechowski.kazimierz.it@gmail.com>
2020-02-01 23:17:38 +0100

    add graph matching parameter setup

--- AStarGraphNode/GraphMatchingParameters.cs
+++ AStarGraphNode/GraphMatchingParameters.cs
@@ -5,30 +5,114 @@ namespace AStarGraphNode
 {
     public class GraphMatchingParameters<V, VA, EA>
     {
-        public Func<VA, double> vertexAdd;
-        public Func<VA, VA, double> vertexRelabel;
-        public Func<VA, double> vertexRemove;
-        public Func<EA, double> edgeAdd;
-        public Func<EA, EA, double> edgeRelabel;
-        public Func<EA, double> edgeRemove;
-        public IEnumerable<double> aCollection;
-        public IEnumerable<double> bCollection;
+        public Func<VA, double> vertexAdd = a => 1;
+        public Func<VA, double> vertexRemove = a => 1;
+        public Func<VA, VA, double> vertexRelabel = (a1, a2) => a1.Equals(a2) ? 0 : 1;
+
+        public Func<EA, double> edgeAdd = a => 1;
+        public Func<EA, double> edgeRemove = a => 1;
+        public Func<EA, EA, double> edgeRelabel = (a1, a2) => a1.Equals(a2) ? 0 : 1;
+
+        public IEnumerable<double> aCollection = new double[] { .5 };
+        public IEnumerable<double> bCollection = new double[] { .5 };
+
         public List<(V, V)> preassignedVertices = default;
         public GraphEncodingMethod encodingMethod = GraphEncodingMethod.Wojciechowski;
 
-        public static GraphMatchingParameters<V, VA, EA> UnitCostDefault()
+        public static GraphMatchingParameters<V, VA, EA> UnitCostDefault() => new GraphMatchingParameters<V, VA, EA>();
+        public static GraphMatchingParameters<V, double, double> DoubleCostComposer(
+            CostType vertexCostType,
+            CostType edgeCostType,
+            List<object> vertexParameters = default,
+            List<object> edgeParameters = default
+            )
         {
-            return new GraphMatchingParameters<V, VA, EA>()
+            var parameters = new GraphMatchingParameters<V, double, double>();
+
+            Func<List<object>, int, object, object> getParameterDefault = (list, index, defaultValue) =>
             {
-                vertexAdd = a => 1d,
-                edgeAdd = a => 1d,
-                vertexRemove = a => 1d,
-                edgeRemove = a => 1d,
-                vertexRelabel = (a1, a2) => 1d,
-                edgeRelabel = (a1, a2) => 1d,
-                aCollection = new double[] { .5 },
-                bCollection = new double[] { .5 },
+                if (index < list.Count)
+                    return list[index];
+                else
+                    return defaultValue;
             };
+
+            Func<double, double, double, double> bound = (a, c1, c2) =>
+            {
+                var aScaled = a * c2;
+                if (a < 1)
+                    return c1 * aScaled * (1 + aScaled);
+                else
+                    return c1 / (1 / aScaled + 1);
+            };
+
+            switch (vertexCostType)
+            {
+                case CostType.UnitCost:
+                    parameters.vertexAdd = a => 1;
+                    parameters.vertexRemove = a => 1;
+                    parameters.vertexRelabel = (a1, a2) => a1.Equals(a2) ? 0 : 1;
+
+                    parameters.edgeAdd = a => 1;
+                    parameters.edgeRemove = a => 1;
+                    parameters.edgeRelabel = (a1, a2) => a1.Equals(a2) ? 0 : 1;
+
+                    break;
+                case CostType.AbsoluteValue:
+                    parameters.vertexAdd = a => Math.Abs(a);
+                    parameters.vertexRemove = a => Math.Abs(a);
+                    parameters.vertexRelabel = (a1, a2) => Math.Abs(a1 - a2);
+
+                    parameters.edgeAdd = a => Math.Abs(a);
+                    parameters.edgeRemove = a => Math.Abs(a);
+                    parameters.edgeRelabel = (a1, a2) => Math.Abs(a1 - a2);
+
+                    break;
+                case CostType.AbsoluteValueBounded:
+                    parameters.vertexAdd = a => bound(
+                        Math.Abs(a),
+                        (double)getParameterDefault(vertexParameters, 0, 1),
+                        (double)getParameterDefault(vertexParameters, 1, 1)
+                        );
+                    parameters.vertexRemove = a => bound(
+                        Math.Abs(a),
+                        (double)getParameterDefault(vertexParameters, 0, 1),
+                        (double)getParameterDefault(vertexParameters, 1, 1)
+                        );
+                    parameters.vertexRelabel = (a1, a2) => bound(
+                        Math.Abs(a1 - a2),
+                        (double)getParameterDefault(vertexParameters, 0, 1),
+                        (double)getParameterDefault(vertexParameters, 1, 1)
+                        );
+
+
+                    parameters.edgeAdd = a => bound(
+                        Math.Abs(a),
+                        (double)getParameterDefault(edgeParameters, 0, 1),
+                        (double)getParameterDefault(edgeParameters, 1, 1)
+                        );
+                    parameters.edgeRemove = a => bound(
+                        Math.Abs(a),
+                        (double)getParameterDefault(edgeParameters, 0, 1),
+                        (double)getParameterDefault(edgeParameters, 1, 1)
+                        );
+                    parameters.edgeRelabel = (a1, a2) => bound(
+                        Math.Abs(a1 - a2),
+                        (double)getParameterDefault(edgeParameters, 0, 1),
+                        (double)getParameterDefault(edgeParameters, 1, 1)
+                        );
+
+                    break;
+            }
+
+            return parameters;
         }
+    }
+
+    public enum CostType
+    {
+        UnitCost,
+        AbsoluteValue,
+        AbsoluteValueBounded
     }
 }
