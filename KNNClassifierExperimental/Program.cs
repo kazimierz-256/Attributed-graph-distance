2a20204a27a8377929a6670ca32bdfc469bd284b
Kazimierz Wojciechowski <wojciechowski.kazimierz.it@gmail.com>
2020-02-01 18:04:33 +0100

    add a dataset builder

--- KNNClassifierExperimental/Program.cs
+++ KNNClassifierExperimental/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Postgres_enron_database.Data;
+using KNNClassifier;
 
 namespace KNNClassifierExperimental
 {
@@ -10,47 +11,97 @@ namespace KNNClassifierExperimental
         static void Main(string[] args)
         {
             using var context = new EnronContext();
-            var beginDate = DateTime.Parse("1999 Dec 01");// first occurrence of exchanging more than 3000 emails per month
-            var bankruptcyDate = DateTime.Parse("2001 Dec 03");
-            var daySplittingTimeAfter0000hrs = TimeSpan.FromHours(4);
-            var years = Enumerable.Range(2000, 2);
-            var emails = context.Emails;
-            var emailsLocal = emails.AsEnumerable();
-            Console.WriteLine(context.Emails.Count());
+            var dataset = GenerateDataSet(context);
+            System.Console.WriteLine(dataset);
+
+            Console.WriteLine(emails.Count());
+            // var emailsLocal = emails.AsEnumerable();
             // foreach (var group in emailsLocal.GroupBy(email => (email.SendDate.Year, email.SendDate.Month)).OrderBy(group => group.Key))
             // {
             //     System.Console.WriteLine($"{group.Key}: {group.Count()}");
             // }
-            var viableTimespan = ("1999 Dec 01", "2001 Jun 01");
-            var bankruptcyTimespan = ("2001 Nov 01", "2001 Dec 03");
+            // var bankruptcyDate = DateTime.Parse("2001 Dec 03");
+            // var bankruptcyTimespan = (DateTime.Parse("2001 Nov 01"), bankruptcyDate);
+        }
+        private enum DataSetCategory
+        {
+            Training,
+            Validation,
+            Testing
+        }
 
-            var trainingProportion = 5;
-            var validatingProportion = 2;
-            var testingProportion = 3;
+        public static DataSet<string, double, double> GenerateDataSet(
+            EnronContext context,
+            TimeSpan? daySplittingTimeAfter0000hrs = null,
+            TimeSpan? dayLength = null,
+            double trainingProportion = 7,
+            double validatingProportion = 2,
+            double testingProportion = 3
+            )
+        {
+            if (!daySplittingTimeAfter0000hrs.HasValue)
+                daySplittingTimeAfter0000hrs = TimeSpan.FromHours(4);
+            if (!dayLength.HasValue)
+                dayLength = TimeSpan.FromHours(24);// could be longer than 24 hrs, but that's complicated
+            var emails = context.Emails;
 
-            Func<DateTime, int> datetimeToClass = datetime =>
+            Func<DateTime, DataSetCategory> datetimeToClass = datetime =>
             {
-                var date = datetime - daySplittingTimeAfter0000hrs;
+                var date = datetime - daySplittingTimeAfter0000hrs.Value;
                 var random = new Random(date.Day + 31 * date.Month + 366 * date.Year);
-                var totalProportion = (double)(testingProportion + validatingProportion + testingProportion);
+                var totalProportion = (double)(trainingProportion + validatingProportion + testingProportion);
                 var trainingThreshold = trainingProportion / totalProportion;
                 var validatingThreshold = trainingThreshold + validatingProportion / totalProportion;
+
                 var sample = random.NextDouble();
                 if (sample < trainingThreshold)
-                    return 0;
+                    return DataSetCategory.Training;
                 else if (sample < validatingThreshold)
-                    return 1;
+                    return DataSetCategory.Validation;
                 else
-                    return 2;
+                    return DataSetCategory.Testing;
             };
 
-            var randomDay = $"2000 Mar 20";
-            var randomDateFrom = DateTime.Parse(randomDay) + daySplittingTimeAfter0000hrs;
-            var randomDateTo = randomDateFrom.AddHours(24);
-            var emailsFromRandomDay = emails.Where(email => email.SendDate > randomDateFrom && email.SendDate < randomDateTo);
+            var viableTimespan = (
+                    from: DateTime.Parse("1999 Dec 01"),// first occurrence of exchanging more than 3000 emails per month
+                    to: DateTime.Parse("2001 Jun 01")
+                );
+            var beginDate = viableTimespan.from + daySplittingTimeAfter0000hrs.Value;
+            var endDate = viableTimespan.to + daySplittingTimeAfter0000hrs.Value;
 
-            var graph = EmailToGraph.GetGraph(context, emailsFromRandomDay);
+            var dataSetToReturn = new DataSet<string, double, double>();
 
+            for (
+                DateTime date = beginDate, dateEnd = beginDate + dayLength.Value;
+                date < endDate;
+                date += TimeSpan.FromHours(24), dateEnd += dayLength.Value
+                )
+            {
+                var emailsFromDate = emails.Where(email => email.SendDate > date && email.SendDate < dateEnd);
+                var graph = EmailToGraph.GetGraph(context, emailsFromDate);
+
+                switch (datetimeToClass(date))
+                {
+                    case DataSetCategory.Training:
+                        dataSetToReturn.trainingSet.Add(graph);
+                        break;
+                    case DataSetCategory.Validation:
+                        dataSetToReturn.validationSet.Add(graph);
+                        break;
+                    case DataSetCategory.Testing:
+                        dataSetToReturn.testSet.Add(graph);
+                        break;
+                    default:
+                        throw new NotImplementedException("Unknown dataset category");
+                }
+
+#if DEBUG
+                System.Console.Write($"Finished {(date - beginDate).TotalDays * 100d / (endDate - beginDate).TotalDays:f2}%. ");
+                System.Console.WriteLine(dataSetToReturn);
+#endif
+            }
+
+            return dataSetToReturn;
         }
     }
 }
