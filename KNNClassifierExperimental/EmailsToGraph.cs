4d1016089e2b8d0229de596da5a3ddadf24e6ecf
Kazimierz Wojciechowski <wojciechowski.kazimierz.it@gmail.com>
2020-02-01 15:25:27 +0100

    add knn classifier experimental setup

---
+++ KNNClassifierExperimental/EmailsToGraph.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using AttributedGraph;
+using Postgres_enron_database.Models;
+using Postgres_enron_database.Data;
+using System.Linq;
+
+namespace KNNClassifierExperimental
+{
+    public static class EmailToGraph
+    {
+        public static Graph<string, double, double> GetGraph(EnronContext context, IQueryable<EmailObject> emails)
+        {
+            var graph = new Graph<string, double, double>(directed: true);
+            var enronAddresses = new HashSet<string>();
+            var directedEmails = new Dictionary<(string, string), List<EmailObject>>();
+
+            foreach (var email in emails.ToList())
+            {
+                var fromAddress = context.EmailAddresses.Where(ea => ea.Id == email.FromId).First();
+                if (fromAddress.BelongsToEnron)
+                {
+                    enronAddresses.Add(fromAddress.Address);
+                    foreach (var emailTo in context.DestinationEmails
+                        .Where(de => de.EmailId == email.Id)
+                        .Select(de => context.EmailAddresses.Where(ea => ea.Id == de.EmailAddressId).First())
+                        .Where(ea => ea.BelongsToEnron))
+                    {
+                        enronAddresses.Add(emailTo.Address);
+
+                        var key = (fromAddress.Address, emailTo.Address);
+
+                        if (directedEmails.ContainsKey(key))
+                        {
+                            // the same email could have been cloned to a different directory
+                            if (directedEmails[key].FirstOrDefault(em => em.SendDate == email.SendDate) == default)
+                               directedEmails[key].Add(email);
+                        }
+                        else
+                            directedEmails.Add(key, new List<EmailObject>() { email });
+                    };
+                }
+            }
+            // throw new NotImplementedException();
+
+            return graph;
+        }
+
+    }
+}
--- KNNClassifierExperimental/Program.cs
+++ KNNClassifierExperimental/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Postgres_enron_database.Data;
 
@@ -9,11 +10,48 @@ namespace KNNClassifierExperimental
         static void Main(string[] args)
         {
             using var context = new EnronContext();
-            var dateFrom = DateTime.Parse("2002 Dec 01");
-            var dateTo = DateTime.Parse("2002 Dec 01");
-            Console.WriteLine(context.Emails.Select(
-                email => email.SendDate < dateTo && email.SendDate > dateFrom
-                ).Count());
+            var beginDate = DateTime.Parse("1999 Dec 01");// first occurrence of exchanging more than 3000 emails per month
+            var bankruptcyDate = DateTime.Parse("2001 Dec 03");
+            var daySplittingHour = 4;
+            var years = Enumerable.Range(2000, 2);
+            var emails = context.Emails;
+            var emailsLocal = emails.AsEnumerable();
+            Console.WriteLine(context.Emails.Count());
+            // foreach (var group in emailsLocal.GroupBy(email => (email.SendDate.Year, email.SendDate.Month)).OrderBy(group => group.Key))
+            // {
+            //     System.Console.WriteLine($"{group.Key}: {group.Count()}");
+            // }
+            var viableTimespan = ("1999 Dec 01", "2001 Jun 01");
+            var bankruptcyTimespan = ("2001 Nov 01", "2001 Dec 03");
+
+            var trainingProportion = 5;
+            var validatingProportion = 2;
+            var testingProportion = 3;
+
+            Func<DateTime, int> datetimeToClass = datetime =>
+            {
+                var date = datetime - TimeSpan.FromHours(daySplittingHour);
+                var random = new Random(date.Day + 31 * date.Month + 366 * date.Year);
+                var totalProportion = (double)(testingProportion + validatingProportion + testingProportion);
+                var trainingThreshold = trainingProportion / totalProportion;
+                var validatingThreshold = trainingThreshold + validatingProportion / totalProportion;
+                var sample = random.NextDouble();
+                if (sample < trainingThreshold)
+                    return 0;
+                else if (sample < validatingThreshold)
+                    return 1;
+                else
+                    return 2;
+            };
+
+            var randomDay = $"2000 Mar 20 {daySplittingHour}:00";
+            var randomDateFrom = DateTime.Parse(randomDay);
+            var randomDateTo = randomDateFrom.AddHours(24);
+            var emailsFromRandomDay = emails.Where(email => email.SendDate > randomDateFrom && email.SendDate < randomDateTo);
+            System.Console.WriteLine(emailsFromRandomDay.Count());
+
+            var graph = EmailToGraph.GetGraph(context, emailsFromRandomDay);
+
         }
     }
 }
