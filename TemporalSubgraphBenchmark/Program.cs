d093d8692d9cf8fbe65a5996c952d05565084e82
Kazimierz Wojciechowski <wojciechowski.kazimierz.it@gmail.com>
2020-08-26 12:36:41 +0200

    Add temporal subgraph benchmark

---
+++ TemporalSubgraphBenchmark/Program.cs
@@ -0,0 +1,29 @@
+﻿using AStar;
+using RandomGraphProvider;
+using System;
+using TemporalSubgraph;
+
+namespace TemporalSubgraphBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random(3_14159265);
+            double vertexAttributeGenerator() => random.NextDouble();
+            double edgeAttributeGenerator() => random.NextDouble();
+            var nodes = 10;
+            var density = .5d;
+            var randomTemporalGraph1 = RandomGraphFactory.GenerateRandomInstance(nodes, density, true, vertexAttributeGenerator, edgeAttributeGenerator);
+            var randomTemporalGraph2 = RandomGraphFactory.GenerateRandomInstance(nodes, density, true, vertexAttributeGenerator, edgeAttributeGenerator);
+            var initialNode = new TemporalMatchingNode<int, double, double>(randomTemporalGraph1, randomTemporalGraph2);
+            var aStarAlgorithm = new AStarAlgorithm(initialNode);
+
+            while (aStarAlgorithm.Queue.Count > 0)
+            {
+                aStarAlgorithm.ExpandBestNode();
+                var bestNode = aStarAlgorithm.BestNode;
+            }
+        }
+    }
+}
