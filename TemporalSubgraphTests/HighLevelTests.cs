9d94100dc4e910a76ef56b48e1717abd332c55c3
Kazimierz Wojciechowski <wojciechowski.kazimierz.it@gmail.com>
2020-09-05 10:44:15 +0200

    Add a super-subgraph test case

--- TemporalSubgraphTests/HighLevelTests.cs
+++ TemporalSubgraphTests/HighLevelTests.cs
@@ -1,6 +1,9 @@
 using AStar;
 using AttributedGraph;
+using RandomGraphProvider;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using TemporalSubgraph;
 using TemporalSubgraph.Heuristics;
 using Xunit;
@@ -47,6 +50,83 @@ namespace TemporalSubgraphTests
             Assert.Equal("1", temporalMatching.Matching("B"));
             Assert.Equal("2", temporalMatching.Matching("C"));
             Assert.Equal("3", temporalMatching.Matching("E"));
+        }
+
+        public static IEnumerable<object[]> SubgraphSupergraphCases(string name)
+        {
+            var densitiyCount = 8;
+            var verticesCount = 8;
+            foreach (var density in Enumerable.Range(1, densitiyCount).Select(integer => integer * 1d / densitiyCount))
+            {
+                foreach (var supergraphVertexCount in Enumerable.Range(2, verticesCount))
+                {
+                    foreach (var subgraphVertexCount in Enumerable.Range(0, supergraphVertexCount))
+                    {
+                        var random = new Random(subgraphVertexCount + verticesCount * supergraphVertexCount + (int)(density * 10000));
+                        var random2 = new Random(random.Next());
+                        Func<double> edgeAttributeGenerator = () => random.NextDouble();
+                        var (subgraph, supergraph) = RandomGraphFactory.GenerateRandomInstanceWithASubinstance(subgraphVertexCount, supergraphVertexCount, density, true, () => 0, edgeAttributeGenerator, random2, allowLoops: false);
+                        var subgraphPermuted = Transform.PermuteClone(subgraph);
+
+                        yield return new object[] { $"SubgraphVC: {subgraphVertexCount}, supergraphVC: {supergraphVertexCount}, density: {density:0.00}", subgraphPermuted, supergraph };
+                    }
+                }
+            }
+        }
+
+        public static IEnumerable<object[]> IsomorphicGraphCases(string name)
+        {
+            var densitiyCount = 10;
+            var verticesCount = 10;
+            foreach (var density in Enumerable.Range(1, densitiyCount).Select(integer => integer * 1d / densitiyCount))
+            {
+                foreach (var vertexCount in Enumerable.Range(2, verticesCount))
+                {
+                    var random = new Random(vertexCount + (int)(density * 10000));
+                    var random2 = new Random(random.Next());
+                    Func<double> edgeAttributeGenerator = () => random.NextDouble();
+                    var graph1 = RandomGraphFactory.GenerateRandomInstance(vertexCount, density, true, () => 0, edgeAttributeGenerator, random2, allowLoops: false);
+                    var graph2 = Transform.PermuteClone(graph1);
+
+                    yield return new object[] { $"VertexCount: {vertexCount}, density: {density:0.00}", graph1, graph2 };
+                }
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(IsomorphicGraphCases), "Isomorphic")]
+        public void IsomorphicGraphs(string name, Graph<int, int, double> graph1, Graph<int, int, double> graph2)
+        {
+            // Arrange
+            var heuristic = new TrivialHeuristic<int, double>();
+
+            var initialNode = new TemporalMatchingNode<int, int, double>(graph1, graph2, heuristic, false);
+            var algorithm = new AStarAlgorithm<TemporalMatchingNode<int, int, double>>(initialNode);
+
+            // Act
+            var temporalMatching = algorithm.ExpandRecursively();
+
+            // Assert
+            Assert.Equal(graph1.VertexCount, graph2.VertexCount);
+            Assert.Equal(graph1.VertexCount, -1 * temporalMatching.DistanceFromSource());
+        }
+
+        [Theory]
+        [MemberData(nameof(SubgraphSupergraphCases), "SubSupergraph")]
+        public void SubgraphsSupergraphs(string name, Graph<int, int, double> graph1, Graph<int, int, double> graph2)
+        {
+            // Arrange
+            var heuristic = new TrivialHeuristic<int, double>();
+
+            var initialNode = new TemporalMatchingNode<int, int, double>(graph1, graph2, heuristic, false);
+            var algorithm = new AStarAlgorithm<TemporalMatchingNode<int, int, double>>(initialNode);
+
+            // Act
+            var temporalMatching = algorithm.ExpandRecursively();
+
+            // Assert
+            Assert.True(graph1.VertexCount <= graph2.VertexCount);
+            Assert.Equal(graph1.VertexCount, -1 * temporalMatching.DistanceFromSource());
         }
     }
 }
